c8895dd792591daf13a863cd36e4d000ff234802
Manish Bhattacharya <bmanish001@gmail.com>
2016-01-17 18:54:13 +0530

    Update instahack.py
    Scrapping meta tag instead of scrapping the complicated data . We need 9th meta tag  from the image URL

--- instahack.py
+++ instahack.py
@@ -1,27 +1,21 @@
-from flask import Flask,render_template,request 
+from flask import Flask,render_template,request #importing Flask elements
 import requests
 from bs4 import BeautifulSoup
-import json
 app= Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
 	if request.method=='POST' and 'iurl' in request.form:
-		url=request.form['iurl'] #getting url from the template Form
-		raw=requests.get(url) #making request to the URL
-		soup=BeautifulSoup(raw.text,'html.parser') #scrap the page data
+		url=request.form['iurl'] #url from the form
+		raw=requests.get(url) #URL from form goes inside requests
+		soup=BeautifulSoup(raw.text,'html.parser')#scraped the HTML
 
-		data= soup.find_all('script')[5].contents #from list of scripts,we need 5th script tag's content
-		info=data[0] #contents look like JSON array, we needed the first element
-		json_data=json.loads(info[21:-1]) #for making perfect JSON syntax, removing {,},[,]
-
+		data= soup.find_all('meta')[9] #we want 9th meta tag
+		image= str(data)[15:124] #convetered into string and strip it for the image URL 
 
 
-		final_data=json_data['entry_data']['PostPage'] #we needed display_src from nested JSON fields
-		final=final_data[0]
-		image=final['media']['display_src']
 		while image!='':
-			return "<img src="+image+" align='center'>" #render link in img tag
+			return '<img src="'+image+ 'align="center">' #render inside the image tag
 
 	return render_template('index.html')
 
